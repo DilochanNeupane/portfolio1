de9448e21d02b77224653c1222769fe4f8a38210
DilochanNeupane <dilochanneupane@gmail.com>
2024-08-23 22:24:32 +0545

    index.html2

--- script.js
+++ script.js
@@ -1,64 +1,53 @@
-/*==================== toggle icon navbar ====================*/
+// toggle icon navber
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-};
+}
 
 
-/*==================== scroll sections active link ====================*/
+// scroll sections
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let navlinks = document.querySelectorAll('header nav a');
+
 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
+        let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-               links.classList.remove('active');
-               document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        if(top >= offset && top < offset + height ) {
+            // active navbar links
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
             });
-        };
+            // acttive section for animation on scrool
+            sec.classList.add('show-animate');
+            
+            
+        }
+        // if want to use animation that repeates on scroll use this
+         else {
+            sec.classList.remove('show-animate');
+         }
     });
-
-    /*==================== sticky navbar ====================*/
-     
+    // sticky header
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
-    
-    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+
+    // remove toggle icon and navbar when click navbar links (scroll)
 
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
-};
 
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
 
-
-/*==================== scroll reveal ====================*/
-ScrollReveal({
-    //reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
-});
-ScrollReveal().reveal('.home-content, .heading',{ origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{ origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img ',{ origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content ',{ origin: 'right' });
-
-
-/*==================== typed js ====================*/
-const typed = new Typed('.multiple-text',{
-    strings: ['Science Student', 'Web Developer', 'YouTuber'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+}
